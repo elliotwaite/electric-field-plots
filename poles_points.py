7d0352bf83f73d37a6d14c888f559dfe294fecd9
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2021-06-23 21:50:00 -0700

    Add images

--- poles_points.py
+++ poles_points.py
@@ -9,7 +9,7 @@ def E(q, r0, x, y):
     return q * (x - r0[0]) / den, q * (y - r0[1]) / den
 
 
-# Grid of x, y points
+# Grid of x, y points.
 nx, ny = 64, 64
 x = np.linspace(-2, 2, nx)
 y = np.linspace(-2, 2, ny)
@@ -23,7 +23,7 @@ for i in range(nq):
     q = i % 2 * 2 - 1
     charges.append((q, (np.cos(2 * np.pi * i / nq), np.sin(2 * np.pi * i / nq))))
 
-# Electric field vector, E=(Ex, Ey), as separate components
+# Electric field vector, E=(Ex, Ey), as separate components.
 Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
 for charge in charges:
     ex, ey = E(*charge, x=X, y=Y)
@@ -33,7 +33,7 @@ for charge in charges:
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-# Plot the streamlines with an appropriate colormap and arrow style
+# Plot the streamlines with an appropriate colormap and arrow style.
 color = 2 * np.log(np.hypot(Ex, Ey))
 ax.streamplot(
     x,
@@ -48,7 +48,7 @@ ax.streamplot(
     arrowsize=1.5,
 )
 
-# Add filled circles for the charges themselves
+# Add filled circles for the charges themselves.
 charge_colors = {True: '#aa0000', False: '#0000aa'}
 for q, pos in charges:
     ax.add_artist(Circle(pos, 0.05, color=charge_colors[q > 0]))
@@ -58,4 +58,5 @@ ax.set_ylabel('$y$')
 ax.set_xlim(-2, 2)
 ax.set_ylim(-2, 2)
 ax.set_aspect('equal')
+plt.savefig('poles_points.png')
 plt.show()
--- poles_square.py
+++ poles_square.py
@@ -9,7 +9,7 @@ def E(q, r0, x, y):
     return q * (x - r0[0]) / den, q * (y - r0[1]) / den
 
 
-# Grid of x, y points
+# Grid of x, y points.
 nx, ny = 64, 64
 x = np.linspace(-2, 2, nx)
 y = np.linspace(-2, 2, ny)
@@ -36,7 +36,7 @@ for i in range(nq):
     q = -1
     charges.append((q, (x_, y_)))
 
-# Electric field vector, E=(Ex, Ey), as separate components
+# Electric field vector, E=(Ex, Ey), as separate components.
 Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
 for charge in charges:
     ex, ey = E(*charge, x=X, y=Y)
@@ -46,7 +46,7 @@ for charge in charges:
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-# Plot the streamlines with an appropriate colormap and arrow style
+# Plot the streamlines with an appropriate colormap and arrow style.
 color = 2 * np.log(np.hypot(Ex, Ey))
 ax.streamplot(
     x,
@@ -61,7 +61,7 @@ ax.streamplot(
     arrowsize=0.5,
 )
 
-# Add filled circles for the charges themselves
+# Add filled circles for the charges themselves.
 charge_colors = {True: '#aa0000', False: '#0000aa'}
 for q, pos in charges:
     ax.add_artist(Circle(pos, 0.05, color=charge_colors[q > 0]))
@@ -71,4 +71,5 @@ ax.set_ylabel('$y$')
 ax.set_xlim(-2, 2)
 ax.set_ylim(-2, 2)
 ax.set_aspect('equal')
+plt.savefig('poles_square.png')
 plt.show()
